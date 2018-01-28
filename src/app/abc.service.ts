54b35c763b795f0959e867aeaf31ff29a8f42139
manumitsacademy <manumits.info@gmail.com>
2018-01-28 13:19:07 +0530

    commit 2801

---
+++ src/app/abc.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {}
+@Injectable()
+export class AbcService {
+
+  constructor(
+	private httpClient:HttpClient
+  ) { }
+	
+	display(){
+		return "ABSVSVSV";
+	}
+	
+	gettingData():void{
+		this.httpClient.get("http://my-json-server.typicode.com/gubbalapraveen/jdata/posts")
+		.subscribe((res)=>{console.log(res);})
+	}
+}
